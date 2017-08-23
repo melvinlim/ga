0426a2e3792a5f47b730bcdbb8ac664f05ae8c3c
Melvin Lim <melvinlim@MacBook.local>
2017-08-23 14:22:28 -0700

    easier viewing of instructions

--- organism.py
+++ organism.py
@@ -2,5 +2,7 @@ from chromosome import Chromosome
 class Organism(object):
 	def __init__(self,dna=None):
 		self.chromosome=Chromosome(dna)
+	def peek(self):
+		return self.chromosome.rna
 	def step(self,obs):
 		return self.chromosome.execute(obs)
