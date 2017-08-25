55fa8775e1aef0d346196e00b6d3adae49b01b78
Melvin Lim <melvinlim@MacBook.local>
2017-08-25 13:39:13 -0700

    prettier peek formatting

--- organism.py
+++ organism.py
@@ -2,8 +2,13 @@ from chromosome import Chromosome
 class Organism(object):
 	def __init__(self,tables,functions=None):
 		self.chromosome=Chromosome(tables=tables,functions=functions)
-	def peek(self):
-		return self.chromosome.rna
+	def peek(self,i=None):
+		if i!=None:
+			print self.chromosome.rna[i]
+		else:
+			for i in range(len(self.chromosome.rna)):
+				print str(i)+': ',
+				print self.chromosome.rna[i].split('\n')[0]
 	def step(self,obs):
 		return self.chromosome.execute(obs)
 	def assign(self,fitness):
